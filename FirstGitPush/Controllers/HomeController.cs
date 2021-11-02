961c97e7bb8b26cba24da423c579d1d541957588
ZachtMac <zachary.t.mcintosh@outlook.com>
2021-11-02 14:12:45 -0400

    New Controller

---
+++ FirstGitPush/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstGitPush.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private static readonly string name = "Zach McIntosh";
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<HomeClass> Get()
+        {
+            return Enumerable.Range(1, 1).Select(index => new HomeClass
+            {
+                name = name
+            }).ToArray();
+        }
+    }
+}
---
+++ FirstGitPush/HomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstGitPush
+{
+    public class HomeClass
+    {
+        public string name { get; set; }
+
+
+    }
+}
